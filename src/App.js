5d684f1b5a787aa26d3ba4c29c9b149d7a22121f
haripriya17 <haripriyamekala517@gmail.com>
2024-04-10 21:02:23 -0500

    Refs to persist data between renders

--- src/App.js
+++ src/App.js
@@ -264,6 +264,14 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
 
+  const countRef = useRef(0);
+  useEffect(
+    function () {
+      if (userRating) countRef.current = countRef.current + 1;
+    },
+    [userRating]
+  );
+
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
   const watchedUserRating = watched.find(
     (movie) => movie.imdbID === selectedId
@@ -293,6 +301,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       released,
       runtime: Number(runtime.split("").at(0)),
       userRating,
+      countRatingDecisions: countRef.current,
     };
     onAddWatched(newWatchedMovie);
     onCloseMovie();
